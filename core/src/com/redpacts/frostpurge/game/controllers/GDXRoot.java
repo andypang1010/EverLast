cd1b21f2857e209f885b2c1a919a896c864509b3
Andy Pang <cp487@cornell.edu>
2024-02-29 14:16:56 -0500

    Add Logo

--- core/src/com/redpacts/frostpurge/game/controllers/GDXRoot.java
+++ core/src/com/redpacts/frostpurge/game/controllers/GDXRoot.java
@@ -1,6 +1,7 @@
 package com.redpacts.frostpurge.game.controllers;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,14 +13,14 @@ public class GDXRoot extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("redpacts.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 1280 - img.getWidth() / 2, 770 - img.getHeight() / 2);
 		batch.end();
 	}
 	
--- desktop/src/com/redpacts/frostpurge/game/DesktopLauncher.java
+++ desktop/src/com/redpacts/frostpurge/game/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("FrostPurge");
+		config.setWindowedMode(2560, 1440);
+//		config.setFullscreenMode(config.getDisplayMode());
 		new Lwjgl3Application(new GDXRoot(), config);
 	}
 }
