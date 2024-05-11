ecd522a51f69ca6cbf01b83eb9f7b7d6d2935f50
Andy Pang <cp487@cornell.edu>
2024-05-11 19:18:10 -0400

    Some more fix

--- core/src/com/redpacts/frostpurge/game/controllers/GameMode.java
+++ core/src/com/redpacts/frostpurge/game/controllers/GameMode.java
@@ -350,18 +350,14 @@ public class GameMode implements Screen, InputProcessor {
 
                 for (int x = i - 1; x <= i + 1; x++) {
                     for (int y = j - 1; y <= j + 1; y++) {
-                        if (Math.abs((x - i) + (y - j)) != 1  || !currentLevel.inBounds(x, y)) {
-                            continue;
-                        }
-
-//                        if (!currentLevel.inBounds(x, y) || (x == i && y == j)) {
-//                            continue;
-//                        }
-
-                        if (currentLevel.getExtraLayer()[y][x] == null) {
-                            groundedTileGraph.connectTiles(currentTile, currentLevel.getBaseLayer()[y][x]);
-                        } else if (currentLevel.getExtraLayer()[y][x].getType() != TileModel.TileType.OBSTACLE) {
-                            groundedTileGraph.connectTiles(currentTile, currentLevel.getExtraLayer()[y][x]);
+                        System.out.println("X: " + x + ", Y: " + y);
+                        System.out.println("Distance from (" + i + ", " + j + ") is: " + Math.abs((x - i) + (y - j)));
+                        if (Math.abs((x - i) + (y - j)) == 1 && currentLevel.inBounds(x, y)) {
+                            if (currentLevel.getExtraLayer()[y][x] == null) {
+                                groundedTileGraph.connectTiles(currentTile, currentLevel.getBaseLayer()[y][x]);
+                            } else if (currentLevel.getExtraLayer()[y][x].getType() != TileModel.TileType.OBSTACLE) {
+                                groundedTileGraph.connectTiles(currentTile, currentLevel.getExtraLayer()[y][x]);
+                            }
                         }
                     }
                 }
@@ -377,15 +373,11 @@ public class GameMode implements Screen, InputProcessor {
 
                 for (int x = i - 1; x <= i + 1; x++) {
                     for (int y = j - 1; y <= j + 1; y++) {
-                        if (Math.abs((x - i) + (y - j)) != 1  || !currentLevel.inBounds(x, y)) {
-                            continue;
+                        System.out.println("X: " + x + ", Y: " + y);
+                        System.out.println("Distance from (" + i + ", " + j + ") is: " + Math.abs((x - i) + (y - j)));
+                        if (Math.abs((x - i) + (y - j)) == 1 && currentLevel.inBounds(x, y)) {
+                            ignoreObstaclesTileGraph.connectTiles(currentTile, currentLevel.getBaseLayer()[y][x]);
                         }
-
-//                        if (!currentLevel.inBounds(x, y) || (x == i && y == j)) {
-//                            continue;
-//                        }
-
-                        ignoreObstaclesTileGraph.connectTiles(currentTile, currentLevel.getBaseLayer()[y][x]);
                     }
                 }
             }
