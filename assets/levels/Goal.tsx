082aaae5f6c375b625da749c25ac25ba42b97003
tunile943 <ltn090403@gmail.com>
2024-05-14 13:05:46 -0400

    Add relevant assets

---
+++ assets/levels/Goal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Goal" tilewidth="742" tileheight="611" tilecount="9" columns="9">
+ <image source="../../assets/goal/goalFilmStrip.png" width="7419" height="611"/>
+</tileset>
