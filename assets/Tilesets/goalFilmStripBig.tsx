032e48b2536d94fa0dfe980e3294022877c8e81e
tunile943 <ltn090403@gmail.com>
2024-05-18 12:29:59 -0400

    Remove error film strip

--- assets/Tilesets/goalFilmStripBig.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="bigGoalFilmStrip" tilewidth="512" tileheight="422" tilecount="10" columns="10">
- <image source="../../assets/goal/bigGoalFilmStrip.png" width="5120" height="445"/>
-</tileset>
--- assets/Tilesets/goalFilmStripSmall.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="smallGoalFilmStrip" tilewidth="384" tileheight="499" tilecount="10" columns="10">
- <image source="../../assets/goal/smallGoalFilmStrip.png" width="3840" height="499"/>
-</tileset>
